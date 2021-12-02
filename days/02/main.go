f0160dceeabb7f54bea72b3e2d034bd4bb1155a7
Tristan Honscheid <tristanh@mit.edu>
2021-12-01 22:31:03 -0700

    Day 2 solutions

---
+++ days/02/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func parseCommand(line string) (string, int, bool) {
+	var distance int
+	var direction string
+
+	count, err := fmt.Sscanf(line, "%s %d", &direction, &distance)
+	if count < 2 || err != nil {
+		return "", 0, false
+	}
+	return direction, distance, true
+}
+
+func main() {
+	f_input, err := os.Open("days/02/input-example.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f_input.Close()
+
+	var x, part1_y, part2_y, aim int
+	scanner := bufio.NewScanner(f_input)
+	for scanner.Scan() {
+		direction, distance, success := parseCommand(scanner.Text())
+		if !success {
+			continue
+		}
+
+		switch direction {
+		case "forward":
+			x += distance
+			part2_y += distance * aim
+			break
+		case "up":
+			part1_y -= distance
+			aim -= distance
+			break
+		case "down":
+			part1_y += distance
+			aim += distance
+			break
+		default:
+			panic("Unknown command")
+		}
+	}
+
+	fmt.Printf("Part I: %d\n", x*part1_y)
+	fmt.Printf("Part II: %d\n", x*part2_y)
+}
